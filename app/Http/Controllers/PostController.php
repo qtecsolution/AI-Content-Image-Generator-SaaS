1553f113664eeab76ccad30a76989208543d72b4
nmbabor <nmbabor50@gmail.com>
2023-02-22 18:39:27 +0600

    post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,42 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UseCase;
 use GuzzleHttp\Client;
 use OpenAI\Laravel\Facades\OpenAI;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Validator;
 
 class PostController extends Controller
 {
     public function create(Request $request)
     {
-
-        $cases = [
-            'pro_des' => 'Product Description',
-            'blog' => 'Blog Writing',
-            'social' => 'Social Media',
-            'mail' => 'Mail Writing',
-            'google_seo' => 'Google SEO',
-        ];
+        $cases = UseCase::where('is_published', 1)->pluck('title', 'id');
+        // $cases = [
+        //     'pro_des' => 'Product Description',
+        //     'blog' => 'Blog Writing',
+        //     'social' => 'Social Media',
+        //     'mail' => 'Mail Writing',
+        //     'google_seo' => 'Google SEO',
+        // ];
 
         return view('posts.create', compact('cases', 'request'));
     }
     /* Open AI Content Generate */
     public function openAi(Request $request)
     {
-        if (isset($request->prompt)) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'keywords' => 'required',
+            'use_case' => 'required',
+            'temp' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation errors',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        try {
+            // Get use case prompt & replcae placeholder
+            $case = UseCase::findOrFail($request->use_case);
+            $placeholder = array("[keywords]", "[title]", "[description]");
+            $replaceBy = array($request->keywords, $request->title, $request->description);
+             $prompt = str_replace($placeholder, $replaceBy, $case->prompt);
+
             $temp = floatval($request->temp ?? 0.7);
-           // return response()->json($temp, 200);
             $result = OpenAI::completions()->create([
                 'model' => 'text-davinci-003',
-                'prompt' => $request->prompt,
+                'prompt' => $prompt,
                 'temperature' => $temp,
                 'max_tokens' => 2566
             ]);
             $content =  $result['choices'][0]['text'] ?? '';
-            return response()->json($content, 200);
-        } else {
-            return response()->json("I don't get any hints.", 200);
+            $wordCount = str_word_count($content);
+            $charCount = strlen($content);
+            $results = [
+                'content' => $content,
+                'words' => $wordCount,
+                'characters' => $charCount
+            ];
+
+            return response()->json($results, 200);
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong',
+                'error' => $errorMessage,
+            ], 400);
+            return response()->json($results, 200);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'keywords' => 'required',
+            'use_case_id' => 'required',
+            'generated_content' => 'required',
+        ]);
+        return $request->all();
+        
+
+        try {
+            
+
+            return back();
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+            
+            return response()->json($results, 200);
         }
     }
 
@@ -69,7 +127,8 @@ class PostController extends Controller
         Storage::put('public/images/image.jpg', $image_data);
     }
 
-    public function default(){
+    public function default()
+    {
         return view('default');
     }
 }
