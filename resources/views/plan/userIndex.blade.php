ef2e95e06b00371fa7848fc90f10102ebf9ac37d
islamrumon <islamrumon707@gmail.com>
2023-02-27 18:28:10 +0600

    work with plan

--- resources/views/plan/userIndex.blade.php
+++ resources/views/plan/userIndex.blade.php
@@ -10,36 +10,76 @@
                       
                     </div>
                     <div class="card-body">
-                        <table class="table">
-                            <thead>
-                              <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name of package</th>
-                                <th scope="col">package Price</th>
-                                <th scope="col">Action</th>
-                              </tr>
-                            </thead>
-                            <tbody>
+
+                        <div class="pricing-body">
+
+                            <div class="pricing-cards">
+                                <!-- single  card free  -->
                                 @foreach ($plans as $item)
-                                <tr>
-                                    <th scope="row">{{$loop->index +1}}</th>
-                                    <td>{{$item->name}}</td>
-                                    <td>{{$item->price}}</td>
-                                    <td>
-                                        @if($user->plan_id == $item->id)
-                                        <a href="{{route('plan.expanse',$item->id)}}">See The Expanses</a>
-                                        @else
-                                        <a href="{{route('plan.purchase',$item->id)}}">Purchase now</a>
-                                        @endif
+                                <div class="pricing-card">
+
+                                    <div class="pricing-card-header">
+                                        <span class="price">
+                                            <span class="currency">{{readConfig('currency_sambol')}}</span>
+                                            <span class="number">{{$item->price}}</span>
+                                            <span class="plane-time">/mo</span>
+                                        </span>
+                                        <span class="name">{{$item->name}}</span>
+                                        <p class="info {{strpos($item->sub_name, "@") !== false ? 'text-secondary' : ''}}">{{$item->sub_name}}</p>
+                                    </div>
+
+                                    <div class="pricing-card-body">
+                                        <ul class="facility-list">
+                                            <li>
+                                                <span class="icon-wrapper">
+                                                    <img src="{{asset('assets/images/icons/check.svg')}}" alt="check icon ">
+                                                </span>
+                                                <span>Generate Maximum {{$item->word_count}} AI Words</span>
+                                            </li>
+                                            <li>
+                                                <span class="icon-wrapper">
+                                                    <img src="{{asset('assets/images/icons/check.svg')}}" alt="check icon ">
+                                                </span>
+                                                <span>{{$item->call_api_count}} Api Request / month</span>
+                                            </li>
+                                            <li>
+                                                <span class="icon-wrapper">
+                                                    <img src="{{asset('assets/images/icons/check.svg')}}" alt="check icon ">
+                                                </span>
+                                                <span>Store {{$item->documet_count}} documents on server</span>
+                                            </li>
+                                            <li>
+                                                <span class="icon-wrapper">
+                                                    <img src="{{asset('assets/images/icons/check.svg')}}" alt="check icon ">
+                                                </span>
+                                                <span>Store {{$item->image_count}} Generate Image on server</span>
+                                            </li>
+                                            <li>
+                                                <span class="icon-wrapper cross-icon">
+                                                    <img src="{{asset('assets/images/icons/check.svg')}}" alt="cross  icon ">
+                                                </span>
+                                                <span>Support {{Str::upper($item->lang)}} Languages</span>
+                                            </li>
+                                        </ul>
+                                        <div class="d-grid">
+                                            @if($user->plan_id == $item->id)
+                                            <button type="button" class="btn-subscribe subscribed" onclick="redirectUrl('{{route('plan.expanse',$item->id)}}')"> 
+                                                See The Expanses
+                                            </button>
+                                            @else
+                                            <button type="button" class="btn-subscribe" onclick="redirectUrl('{{route('plan.purchase',$item->id)}}')"> 
+                                                 Purchase now
+                                            </button>
+                                            @endif
                                         
-                                       
-                                    </td>
-                                  </tr>
+                                        </div>
+                                    </div>
+
+                                </div>
                                 @endforeach
-                             
-                              
-                            </tbody>
-                          </table>
+                            </div>
+                        </div>
+                    
                     </div>
                 </div>
             </div>
